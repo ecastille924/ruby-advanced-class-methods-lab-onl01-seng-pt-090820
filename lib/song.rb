87ea89d529a7b61d910388c25b537b7db8952043
Erick Castille <ecastille924@gmail.com>
2020-09-28 23:46:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,11 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    
+    song = self.new
+    song.name = (filename.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
   end
   
 end
