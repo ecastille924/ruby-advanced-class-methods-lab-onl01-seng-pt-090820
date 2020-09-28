f432f3af1742c617995bc5ce0a3afc8a59b01874
Erick Castille <ecastille924@gmail.com>
2020-09-28 23:53:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,8 @@ class Song
   def self.new_from_filename(filename)
     split_filename = filename.chop.chop.chop.chop.split(" - ")
     song = Song.new
-    song.name = 
+    song.name = split_filename[0]
+    
   end
   
 end
